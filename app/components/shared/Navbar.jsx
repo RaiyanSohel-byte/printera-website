4f076537bab8f8fc76555f9818d3fb90493ada2d
RaiyanSohel-byte <raiyansohel22@gmail.com>
2026-02-22 22:14:42 +0600

    navlinks added

--- app/components/shared/Navbar.jsx
+++ app/components/shared/Navbar.jsx
@@ -4,6 +4,7 @@ import logo from "../../../public/logo.png";
 import cart from "../../../public/cart.png";
 import user from "../../../public/user.png";
 import MobileNavbar from "./MobileNavbar";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-4 lg:px-0 max-w-[1200px] mx-auto">
@@ -21,10 +22,30 @@ const Navbar = () => {
       {/* NavLinks */}
       <div className="hidden lg:block">
         <ul className="flex space-x-8 text-[#364153]">
-          <li className="hover:text-red-600 cursor-pointer">Products</li>
-          <li className="hover:text-red-600 cursor-pointer">Promo Catalog</li>
-          <li className="hover:text-red-600 cursor-pointer">Portfolio</li>
-          <li className="hover:text-red-600 cursor-pointer">Request a Quote</li>
+          <Link
+            href={"/products"}
+            className="hover:text-red-600 cursor-pointer"
+          >
+            Products
+          </Link>
+          <Link
+            href={"/promo-catalogue"}
+            className="hover:text-red-600 cursor-pointer"
+          >
+            Promo Catalog
+          </Link>
+          <Link
+            href={"/portfolio"}
+            className="hover:text-red-600 cursor-pointer"
+          >
+            Portfolio
+          </Link>
+          <Link
+            href={"/request-quote"}
+            className="hover:text-red-600 cursor-pointer"
+          >
+            Request a Quote
+          </Link>
         </ul>
       </div>
       <div className="hidden lg:flex items-center justify-between gap-6">
